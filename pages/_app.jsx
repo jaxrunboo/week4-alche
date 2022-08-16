7ce54c24165d49f67ed35d8ff073b96ebcc0a438
bobo <Jaxrunboo@163.com>
2022-08-16 22:10:33 +0800

    f

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: bobo 1052028084@qq.com
+ * @Date: 2022-08-16 19:48:16
+ * @LastEditors: bobo 1052028084@qq.com
+ * @LastEditTime: 2022-08-16 21:50:43
+ * @FilePath: /nameoftheproject/pages/_app.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import '../styles/globals.css'
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+export default MyApp
---
+++ pages/nftCard.jsx
@@ -0,0 +1,30 @@
+/*
+ * @Author: bobo 1052028084@qq.com
+ * @Date: 2022-08-16 21:52:51
+ * @LastEditors: bobo 1052028084@qq.com
+ * @LastEditTime: 2022-08-16 21:53:17
+ * @FilePath: /nameoftheproject/pages/nftCard.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export const NFTCard = ({ nft }) => {
+
+    return (
+        <div className="w-1/4 flex flex-col ">
+        <div className="rounded-md">
+            <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+        </div>
+        <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
+            <div className="">
+                <h2 className="text-xl text-gray-800">{nft.title}</h2>
+                <p className="text-gray-600">Id: {nft.id.tokenId}</p>
+                <p className="text-gray-600" >{nft.contract.address}</p>
+            </div>
+
+            <div className="flex-grow mt-2">
+                <p className="text-gray-600">{nft.description}</p>
+            </div>
+        </div>
+
+    </div>
+    )
+}
